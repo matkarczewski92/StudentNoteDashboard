d1ab9456b2d4c215545251ca0306b29acd50114c
Mateusz <matkarczewski92@gmail.com>
2025-10-14 11:45:44 +0200

    New feature: mail from lecturer

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,56 +24,6 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
-
-    public function groups(): BelongsToMany
-    {
-        return $this->belongsToMany(\App\Models\Group::class);
-    }
-
-    public function events()
-    {
-        return $this->hasMany(\App\Models\Event::class);
-    }
-
-    public function hasAnyRole(array $roles): bool
-    {
-        return in_array($this->role ?? 'user', $roles, true);
-    }
-
-    public function sendPasswordResetNotification($token): void
-    {
-        $this->notify(new ResetPasswordNotification($token));
-    }
-}
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use App\Notifications\ResetPasswordNotification;
-
-class User extends Authenticatable
-{
-    use HasFactory, Notifiable;
-
-    protected $fillable = [
-        'name', 'email', 'album', 'password',
-    ];
-
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
             'password'          => 'hashed',
         ];
     }
